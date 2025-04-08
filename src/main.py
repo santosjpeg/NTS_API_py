244786563c3d0d4486ea1171f185781a4789022d
aaron <santosaaron2004@gmail.com>
2025-04-07 21:07:24 -0400

    Init search collections functionality, prints 12 most recent additions

--- src/main.py
+++ src/main.py
@@ -3,9 +3,26 @@ import time
 import requests
 import json
 
+def display_menu_options():
+    print("Welcome to NTS Radio tools.")
+    print("(1) 24/7 Radio Channels")
+    print("(2) Search Archived Mixes")
+    print("(3) Explore Infinite Mixtapes")
+    print("(exit) Ends Program")
+
 def handle_menu_options(choice):
     if choice == '1':
         play_radio()
+    elif choice == '2':
+        search_archive()
+    elif choice =='3':
+        search_infinite_mixtapes()
+    else:
+        print("Invalid Option: Choose from {1,2,3,exit}")
+
+def handle_search_options(option):
+    print(f"[+] User selected {option}...")
+    pass
 
 def print_current_sets(raw_radio):
     for i in raw_radio:
@@ -19,8 +36,6 @@ def print_current_sets(raw_radio):
         print("DESCRIPTION: " + desc)
         print("GENRES: " + genre_list)
 
-    print('[+] Radio Channel Overview SUCCESS')
-
 def print_upcoming_set(json_output):
     next = json_output['next']
     title = next['broadcast_title']
@@ -31,10 +46,52 @@ def print_upcoming_set(json_output):
     print(f"COMING NEXT: {title}")
     print(f"GENRES: {genre_list}")
 
+
+def search_archive():
+    API_LATEST = 'https://www.nts.live/api/v2/collections/recently-added'
+    with open('latest.json','w', encoding='utf-8') as fp:
+        r = requests.get(API_LATEST)
+        if(r.status_code == 200):
+            fp.write(json.dumps(r.json(), indent=2))
+        else:
+            print("ERROR: Failed to perform GET Request")
+            return
+    
+    with open('latest.json','r', encoding='utf-8') as fp:
+        json_output = json.loads(fp.read())['results']
+        all_results = [i['name'] for i in json_output]
+
+    user_search = ''
+
+    print("RECENTLY ADDED SHOWS:")
+    print("0. SEARCH")
+    for i in range(len(all_results)):
+        print(f"{i+1}. {all_results[i]}")
+    print("(exit) exit")
+
+    while(True):
+        user_search = input("Enter menu option: ")
+        if user_search == 'exit':
+            break
+        handle_search_options(user_search)
+        
+
+def search_infinite_mixtapes():
+    pass
+
 def play_radio():
+    API_RADIO = 'https://www.nts.live/api/v2/live'
+    with open('radio.json','w', encoding='utf-8') as fp:
+        r = requests.get(API_RADIO)
+        if(r.status_code == 200):
+            fp.write(json.dumps(r.json(), indent=2))
+        else:
+            print("ERROR: Failed to perform GET Request")
+            return
+
     NTS_CHANNEL_1 = 'https://stream-relay-geo.ntslive.net/stream'
     NTS_CHANNEL_2 = 'https://stream-relay-geo.ntslive.net/stream2'
-    with open('example.json','r',encoding='utf-8') as fp:
+    with open('radio.json','r',encoding='utf-8') as fp:
         json_output = json.loads(fp.read())['results']
         raw_radio = [i for i in json_output]
 
@@ -46,8 +103,6 @@ def play_radio():
     while(radio_channel != '1' and radio_channel != '2'):
         radio_channel = input("Select radio station 1 or 2: ")
 
-    print("[+] Selection loop successfully exited")
-
     if radio_channel == '1':
         radio_select = NTS_CHANNEL_1
     elif radio_channel == '2':
@@ -57,42 +112,20 @@ def play_radio():
 
     player = vlc.MediaPlayer(radio_select)
     radio_output = raw_radio[int(radio_channel) - 1]
-    print("[+] Player successfully initialized")
-    print()
     player.play()
     time.sleep(1)
     print_upcoming_set(radio_output)
     input("Press enter to stop")
     player.stop()
-    print("[+] Successfully exiting radio...")
 
 def main():
-
-    with open('example.json','w', encoding='utf-8') as fp:
-        r = requests.get('https://www.nts.live/api/v2/live')
-        if(r.status_code == 200):
-            print('[+] HTTP GET on nts.live/radio SUCCESS')
-            fp.write(json.dumps(r.json(), indent=2))
-            print('[+] Write to example.json SUCCESS')
-
-
-
-    MENU_OPTIONS = {'1','2','3'}
     menu_primitive = -1 
     while(True):
-        print("Welcome to NTS Radio tools.")
-        print("(1) 24/7 Radio Channels")
-        print("(2) Search Archived Mixes")
-        print("(3) Explore Infinite Mixtapes")
-        print("(exit) Ends Program")
-
+        display_menu_options()
         menu_primitive = input("Enter valid menu options: ")
         if menu_primitive == 'exit':
             break
-        if menu_primitive not in MENU_OPTIONS:
-            print("Invalid Option: Choose from {1,2,3,exit}")
-        else:
-            handle_menu_options(menu_primitive)
+        handle_menu_options(menu_primitive)
 
 if __name__ == '__main__':
     main()
